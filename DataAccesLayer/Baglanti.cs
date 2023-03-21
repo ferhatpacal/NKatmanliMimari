59cfc3bd7712674aae3eaddcc4e8b928be74bd29
iamfe <iamfe@FERHAT>
2023-03-22 01:20:02 +0300

    Proje dosyası ekle.

---
+++ DataAccesLayer/Baglanti.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DataAccesLayer
+{
+    public class Baglanti
+    {
+        public static SqlConnection bgl = new SqlConnection(@"Data Source=FERHAT\SQLEXPRESS;Initial Catalog=DbPersonel;Integrated Security=True");
+    }
+}
